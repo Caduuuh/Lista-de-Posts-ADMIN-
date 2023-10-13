834b3bee73a118689dd7079ca23e2f74d3d59666
Caduuuh <carloseduardodesouzamattos@gmail.com>
2023-10-13 11:47:05 -0300

    mudanças

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+function openExcluir(){
+    var x = document.getElementById("Excluir");
+    if(x.className === "ExcluirFechado"){
+        x.className = "ExcluirAberto";
+    } else{
+        x.className = "ExcluirFechado";
+    }
+}
+
+function openSim(){
+    var x = document.getElementById("Excluir");
+    if(x.className === "ExcluirFechado"){
+        x.className = "ExcluirAberto";
+    } else{
+        x.className = "ExcluirFechado";
+    }
+}
+
+function openNao(){
+    var x = document.getElementById("Excluir");
+    if(x.className === "ExcluirFechado"){
+        x.className = "ExcluirAberto";
+    } else{
+        x.className = "ExcluirFechado";
+    }
+}
