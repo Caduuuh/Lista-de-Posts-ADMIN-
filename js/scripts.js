952a2a8301868b35905cad88fcf7ba9f7a7757e2
Caduuuh <carloseduardodesouzamattos@gmail.com>
2023-10-20 10:19:26 -0300

    Revisões sprint modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,46 @@
-function openExcluir(){
-    var x = document.getElementById("Excluir");
-    if(x.className === "ExcluirFechado"){
-        x.className = "ExcluirAberto";
-    } else{
-        x.className = "ExcluirFechado";
-    }
+var btnNewPost = document.getElementById("CriarPost");
+var btnViewPost = document.getElementById("view");
+var btnEditPost = document.getElementById("edit");
+var btnDeletePost = document.getElementById("delete");
+
+var modalDelete = document.getElementById("modalDelete");
+var modalNewPost = document.getElementById("modalNewPost");
+var newPostBox = document.getElementById("newPostBox");
+var modalEditPost = document.getElementById("modalEditPost");
+var modalViewPost = document.getElementById("modalViewPost");
+
+
+var closeDelete = document.getElementById("closeDelete")[0];
+var closeNewPost = document.getElementById("closeNewPost")[0];
+
+btnViewPost.onclick=function(){
+    modalViewPost.style.display = "block";
+    newPostBox.style.display = "none";
 }
 
-function openSim(){
-    var x = document.getElementById("Excluir");
-    if(x.className === "ExcluirFechado"){
-        x.className = "ExcluirAberto";
-    } else{
-        x.className = "ExcluirFechado";
-    }
+btnEditPost.onclick=function(){
+    modalEditPost.style.display = "block";
+    newPostBox.style.display = "none";
 }
 
-function openNao(){
-    var x = document.getElementById("Excluir");
-    if(x.className === "ExcluirFechado"){
-        x.className = "ExcluirAberto";
-    } else{
-        x.className = "ExcluirFechado";
+btnNewPost.onclick =function(){
+    modalNewPost.style.display = "block";
+    newPostBox.style.display = "none";
+
+}
+
+
+btnDeletePost.onclick = function(){
+    modalDelete.style.display = "block";
+    newPostBox.style.display = "none";
+}
+
+closeDelete.onclick = function(){
+    modalDelete.style.display = "none";
+}
+
+window.onclick = function(event){
+    if(event.target == modalDelete){
+        modalDelete.style.display = "none";
     }
 }
